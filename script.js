67ca3ed4181f263c6b6c01c5b078623c47b72555
Dhruw <dharmendra23101@iiitnr.edu.in>
2025-02-02 02:05:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ document.getElementById("admin-form").addEventListener('submit', function(e) {
 });
 
 console.log("Form script loaded.");
+document.getElementById("contect").addEventListener('submit', function (e) {
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const mail = document.getElementById("mail").value;
+    const message = document.getElementById("message").value;
+
+
+    const response = { name, mail, message, date: new Date().toISOString() };
+    const responses = JSON.parse(localStorage.getItem('responses')) || [];
+
+    responses.push(response);
+    localStorage.setItem("responses", JSON.stringify(responses));
+    console.log(responses);
+    this.reset();
+});
